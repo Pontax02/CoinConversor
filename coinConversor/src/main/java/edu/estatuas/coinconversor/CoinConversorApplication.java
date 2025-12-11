aa9beb220dc7ba552fb89fd89c7bddf27e1c041c
Pontax02 <ppontanilla07@gmail.com>
2025-12-11 21:10:07 +0100

    init

---
+++ coinConversor/src/main/java/edu/estatuas/coinconversor/CoinConversorApplication.java
@@ -0,0 +1,13 @@
+package edu.estatuas.coinconversor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinConversorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinConversorApplication.class, args);
+    }
+
+}
---
+++ coinConversor/src/test/java/edu/estatuas/coinconversor/CoinConversorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.estatuas.coinconversor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinConversorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
